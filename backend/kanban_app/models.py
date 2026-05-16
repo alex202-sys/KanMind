778ddadc3604339c9c4d3cfff37f957a4c12a291
alex202-sys <aleksandr.ogloblin.job@gmail.com>
2026-05-17 00:42:06 +0200

    Models Boards, Ticket erstelt

--- backend/kanban_app/models.py
+++ backend/kanban_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
+class Boards(models.Model):
+    title = models.CharField(max_length=155)
+    owner_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    member = models.ManyToManyField(User, related_name='members')
+
+    def __str__(self):
+        return self.title
+
+class TicketStatus(models.TextChoices):
+    IN_PROGRESS = 'in-progress', 'In Progress'
+    TODO = 'to-do', 'To-Do'
+    DONE = 'done', 'Done' 
+    REVIEW = 'review', 'Review' 
+     
+
+class TicketPriority(models.TextChoices):
+    HIGH = 'high', 'High'
+    MEDIUM = 'medium', 'Medium'
+    LOW = 'low', 'Low' 
+    # Holt alle Tickets, die fertig sind
+    #finished_tickets = Ticket.objects.filter(status=Ticket.Status.ENDED)
+    # ticket.status = Ticket.Status.IN_PROGRESS
+    # ticket.save()
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=255)
+    boards = models.OneToOneField(Boards, on_delete=models.SET_NULL, null=True)
+    description = models.CharField(max_length=255)
+    status = models.CharField(max_length=20, choice=TicketStatus.choices,  default=TicketStatus.IN_PROGRESS)
+    priority = models.CharField(max_length=20, choice=TicketPriority.choices,  default=TicketPriority.MEDIUM)
+    due_date = models.DateField()
+    assignee = models.OneToOneField(User, on_delete=models.SET_NULL, blank=True, null=True)
+    reviewer = models.OneToOneField(User, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.get_status_display()})"
